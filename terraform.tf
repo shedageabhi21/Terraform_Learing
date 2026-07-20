0cfe685440237c9096953bdec10194f8d57e9cdd
shedageabhi21 <root@localhost.localdomain>
2026-07-20 11:34:14 +0530

    Adding the backend of s3

--- terraform.tf
+++ terraform.tf
@@ -6,6 +6,14 @@ terraform {
     }
   }
 
-  required_version = ">= 1.2"
+  required_version = ">= 1.10"
+
+  backend "s3" {
+    bucket       = "abhishek-my-lab-bucket-for-tfe"
+    key          = "learn-terraform/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
 
